7861cfcb835a1a0a704f6bc86a527ea2dfe47cd3
AustinJoo97 <austinjoo97@gmail.com>
2021-07-26 15:55:25 -0400

    added firstName, lastName, location, bio, and proPic fields at typedefs, resolvers, and User model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -60,8 +60,8 @@ const resolvers = {
   },
 
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
+    addUser: async (parent, { firstName, lastName, username, email, password, location, bio, proPic }) => {
+      const user = await User.create({ firstName, lastName, username, email, password, location, bio, proPic });
       const token = signToken(user);
       return { token, user };
     },
