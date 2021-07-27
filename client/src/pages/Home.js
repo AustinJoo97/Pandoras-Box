0f0e1dae7f19894de9f3056ec9d80ed280cea692
ForestW70 <hexaforest@gmail.com>
2021-07-27 19:07:01 -0400

    added mock queries to home.js. added api file to client/utils.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useQuery } from '@apollo/client';
+
 import RecentlyReleased from '../components/RecentlyReleasedSection';
 import YourFavorites from '../components/YourFavoritesSection';
 import BasedOnFavorites from '../components/BasedOnFavoritesSection';
 import RecentComments from '../components/RecentCommentSection';
-// import { useQuery } from '@apollo/client';
-
-// import { QUERY_LISTS } from '../utils/queries';
+import { QUERY_FAVORITES, QUERY_PREPOP } from '../utils/queries';
+import { searchSpotifyByGenreName } from '../utils/API';
 
 const Home = () => {
-  // const { loading, data } = useQuery(QUERY_LISTS);
-  // const lists = data?.lists || [];
+
+  // to query for a users favorites list. can do without this if unable
+  const { loading, userData } = useQuery(QUERY_FAVORITES);
+  const lists = userData?.lists || [];
+
+  // will most likely need a call for each list from spotify we want to initially generate
+  const HandleApiCalls = async () => {
+    try {
+      const response = await searchSpotifyByGenreName('rock');
+
+      if (!response.ok) {
+        throw new Error('could not fetch!');
+      }
+
+      const { rockAlbums } = await response.json();
+
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   return (
     <main>
       <div className="">
       <RecentlyReleased />
-      <YourFavorites />
+      <YourFavorites data={userData}/>
       <BasedOnFavorites />
       <RecentComments />
       </div>
---
+++ client/src/utils/API.js
@@ -0,0 +1,28 @@
+export const searchSpotifyByGenreName = (query) => {
+    return fetch('some spotify api search')
+}
+
+export const searchSpotifyByArtistName = (query) => {
+    return fetch('some spotify api search')
+}
+
+export const searchSpotifyByAlbumName = (query) => {
+    return fetch('some spotify api search')
+}
+
+export const searchSpotifyByArtistId = (query) => {
+    return fetch('some spotify api search')
+}
+
+export const searchSpotifyByAlbumId = (query) => {
+    return fetch('some spotify api search')
+}
+
+// maybe
+export const searchSpotifyByRecentAlbums = (query) => {
+    return fetch('some spotify api search')
+}
+
+export const searchSpotifyByTopArtists = (query) => {
+    return fetch('some spotify api search')
+}
